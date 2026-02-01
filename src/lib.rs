eb340e31f4e969b0763381a216d744b53ef8f362
dharanish.kedarisetti <dharanish.kedarisetti@gmail.com>
2026-01-31 21:11:28 -0500

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -319,9 +319,7 @@ fn compress_rvl(input: Vec<u16>) -> PyResult<Vec<u8>> {
     let mut codec = RVLCodec::new();
     let mut output = Vec::new();
     if let Err(err) = codec.compress_rvl_checked(&input, &mut output) {
-        return Err(pyo3::exceptions::PyValueError::new_err(format!(
-            "{err:?}"
-        )));
+        return Err(pyo3::exceptions::PyValueError::new_err(format!("{err:?}")));
     }
     Ok(output)
 }
@@ -331,9 +329,7 @@ fn decompress_rvl(input: Vec<u8>, num_pixels: usize) -> PyResult<Vec<u16>> {
     let mut codec = RVLCodec::new();
     let mut output = Vec::new();
     if let Err(err) = codec.decompress_rvl_checked(&input, &mut output, num_pixels) {
-        return Err(pyo3::exceptions::PyValueError::new_err(format!(
-            "{err:?}"
-        )));
+        return Err(pyo3::exceptions::PyValueError::new_err(format!("{err:?}")));
     }
     Ok(output)
 }
